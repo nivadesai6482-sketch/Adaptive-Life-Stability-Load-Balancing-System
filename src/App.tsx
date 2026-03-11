586c45e03516077f0b55bd1c0298e2097b39eaa8
nivadesai6482-sketch <nivadesai6482@gmail.com>
2026-03-11 12:18:45 +0530

    Create dashboard page and integrate with dashboard layout

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { DashboardLayout } from './components/layout/DashboardLayout';
+import { Dashboard } from './pages/Dashboard';
+
+function App() {
+    return (
+        <DashboardLayout>
+            <Dashboard />
+        </DashboardLayout>
+    );
+}
+
+export default App;
